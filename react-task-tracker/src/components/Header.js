9ccd71a98e286982b1a60225486e0630037eb8d1
Amalsaju <amalsaju1998@gmail.com>
2022-07-09 01:08:29 +0530

    Basic Functionality added

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -4,16 +4,16 @@ import Button from './Button'
 
 
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
 
     const onClick = () => {
-        console.log("hello");
+        onAdd()
     }
 
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onClick} />
     </header>
   )
 }
