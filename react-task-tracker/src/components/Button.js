021a69646bf9ef2bd249c939f6b1d9d5d7660e01
Amalsaju <amalsaju1998@gmail.com>
2022-07-09 19:22:33 +0530

    Task Tracker project completed
    -Added fake json server
    -Added functionality for updating data to server
    -Added basic routing functionality

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,5 +1,5 @@
 
-
+import React from "react";
 const Button = ({color, text, onClick}) => {
 
 
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,7 +1,7 @@
-
 import PropTypes from 'prop-types'
 import Button from './Button'
-
+import React from "react";
+import {useLocation} from 'react-router-dom'
 
 
 const Header = ({title, onAdd, showAdd}) => {
@@ -10,10 +10,12 @@ const Header = ({title, onAdd, showAdd}) => {
         onAdd()
     }
 
+const location = useLocation()
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onClick} />
+      {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onClick} />}
     </header>
   )
 }
