be622ea25c79a3a894fdc174083d600ada9bf141
Amalsaju <amalsaju1998@gmail.com>
2022-07-16 18:18:48 +0530

    Basic Structure
    Scaffolding and adding the basic structure of the landing page.

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,15 @@
+
+import Directory from './components/directory/directory.component';
+
+const App = () => {
+
+
+
+  return (
+    <div>
+      <Directory />
+    </div>
+  );
+}
+
+export default App;
