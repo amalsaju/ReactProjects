810c30b41cb347138ed958e7f335575d35e40320
Amalsaju <amalsaju1998@gmail.com>
2022-07-16 20:49:08 +0530

    Routing using React-Router
    Basic Routing functionality implemented using React-Router (v6).
    
    Just created a navigation bar persistent across different pages.

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,14 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 
-import Directory from './components/directory/directory.component';
-
-const App = () => {
-
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
 
 
+const Shop = () => {
   return (
     <div>
-      <Directory />
+      <h2>I am the shop page.</h2>
     </div>
+  );
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Route>
+
+    </Routes>
   );
 }
 
