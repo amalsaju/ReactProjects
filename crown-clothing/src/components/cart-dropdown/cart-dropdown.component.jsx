4b6ba8f8df2b24a42fbb1de26a01e3d2e6ea1613
Amalsaju <amalsaju1998@gmail.com>
2022-07-25 23:34:09 +0530

    Finished adding cart functionality
    Implemented add to cart functionality along with displaying a text when there are no items in cart.

--- crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,29 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+
+  const { cartItems, cartCount } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-      <div className='card-items'>
-      </div>
+      {cartCount === 0 ?
+        (
+          <div className='empty-message'>
+            <span>No items in cart!</span>
+          </div>
+        ) :
+        (
+          <div className='cart-items'>
+            {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
+          </div>
+        )}
       <Button>Go to Checkout</Button>
     </div>
   );
