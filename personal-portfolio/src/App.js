f91352800289aeec160b305e08f25523c2d39395
Amalsaju <amalsaju1998@gmail.com>
2022-09-30 23:33:20 +1000

    Website Header changes
    Changed the website tab logo from react to personal logo.
    Changed title name to "Amal Saju Portfolio".
    Fixed bug where reboot.scss overrode social icon color.

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,21 +1,13 @@
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/navbar/navbar.component";
 import LandingPage from "./components/landing-page/landing-page.component";
 import Skills from "./components/skills/skills.component";
 import Projects from "./components/side-projects/side-projects.component";
 import Footer from "./components/footer/footer.component";
 
-import React, { useState, useEffect } from "react"
-import ThemeToggleButton from './components/theme-toggle-button/theme-toggle-button.component';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
-
-
-
-
   return (
     <div className="App">
       <NavBar />
